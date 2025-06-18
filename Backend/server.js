2519de94628b3d73f84a478fab5691b6f93fe48b
sutharsan112112 <sutharsan112112@gmail.com>
2025-06-18 11:53:42 +0530

    first commit

--- Backend/server.js
+++ Backend/server.js
@@ -22,7 +22,7 @@ app.get('/', (req, res) => {
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api/vehicles', VehicleRoutes);
+app.use('/api/vehicle', VehicleRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/admin', adminRoutes);
 app.use("/api/contact", contactRoutes);
