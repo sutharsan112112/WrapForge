12f1e2c3e63af82f40c4d9895697dfa59055c1bf
sutharsan112112 <sutharsan112112@gmail.com>
2025-06-21 21:04:20 +0530

    first commit

--- Backend/server.js
+++ Backend/server.js
@@ -4,11 +4,11 @@ import dotenv from 'dotenv';
 import connectDB from './config/database.js';
 
 import authRoutes from './routes/authRoutes.js';
-// import VehicleRoutes from './routes/vehicleRoutes.js';
+import VehicleRoutes from './routes/vehicleRoutes.js';
 import adminRoutes from './routes/adminRoutes.js';
-// import contactRoutes from './routes/contactRoutes.js';
+import contactRoutes from './routes/contactRoutes.js';
 import stickerRoutes from './routes/stickerRoutes.js';
-// import galleryRoutes from './routes/galleryRoutes.js';
+import galleryRoutes from './routes/galleryRoutes.js';
 // import paymentRoutes from './routes/paymentRoutes.js';
 
 dotenv.config();
@@ -25,10 +25,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api/auth', authRoutes);
 app.use('/api/admin', adminRoutes);
-// app.use('/api/vehicle', VehicleRoutes);
-// app.use("/api/contact", contactRoutes);
+app.use('/api/vehicle', VehicleRoutes);
+app.use("/api/contact", contactRoutes);
 app.use("/api/sticker", stickerRoutes);
-// app.use('/api/gallery', galleryRoutes);
+app.use('/api/gallery', galleryRoutes);
 // app.use('/api/payment', paymentRoutes);
 
 const PORT = process.env.PORT || 5000;
